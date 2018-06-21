e5ea2154d2eafc69a5cd10deacbaa6d6aebd416b
Ahmet Alp Balkan <ahmetb@google.com>
2018-06-21 10:31:07 -0700

    recommendationservice: add requirements.txt
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- src/recommendationservice/compile_proto.sh
+++ src/recommendationservice/compile_proto.sh
@@ -1,6 +1,6 @@
 # script to compile python protos
 #
 # requires gRPC tools:
-#   python -m pip install grpcio-tools googleapis-common-protos
+#   pip install -r requirements.txt
 
 python -m grpc_tools.protoc -I../../pb --python_out=. --grpc_python_out=. ../../pb/demo.proto
