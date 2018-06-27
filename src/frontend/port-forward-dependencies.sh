3eb02c0679353b5bcef059b507638eaa7cc33e0b
Ahmet Alp Balkan <ahmetb@google.com>
2018-06-27 16:28:16 -0700

    frontend/templates: reenable empty cart
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- src/frontend/port-forward-dependencies.sh
+++ src/frontend/port-forward-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-kubectl port-forward $(kubectl get pods -l app=currencyservice -o=name) 7000:31337 &
-kubectl port-forward $(kubectl get pods -l app=recommendationservice -o=name) 8081:8080 &
-kubectl port-forward $(kubectl get pods -l app=cartservice -o=name) 7070:7070 &
-kubectl port-forward $(kubectl get pods -l app=productcatalogservice -o=name) 3550:3550 &
-kubectl port-forward $(kubectl get pods -l app=checkoutservice -o=name) 5050:5050 &
+kubectl port-forward $(kubectl get pods -l app=currencyservice -o=name | head -n 1) 7000:31337 &
+kubectl port-forward $(kubectl get pods -l app=recommendationservice -o=name | head -n 1) 8081:8080 &
+kubectl port-forward $(kubectl get pods -l app=cartservice -o=name | head -n 1) 7070:7070 &
+kubectl port-forward $(kubectl get pods -l app=productcatalogservice -o=name | head -n 1) 3550:3550 &
+kubectl port-forward $(kubectl get pods -l app=checkoutservice -o=name | head -n 1) 5050:5050 &
 
 set +x
 trap "exit" INT TERM ERR
