0890e8adc44074d64b23e2703cbc8aef6592fa51
Dave Stanke <davidstanke@gmail.com>
2019-01-08 12:09:22 -0500

    On triggered build, write git commit to front end

---
+++ tools/insert_git_commit_link.sh
@@ -0,0 +1,12 @@
+# when project is built by Cloud Build from a source trigger, the 
+# commit SHA will be available as an env var
+
+if [[ "$COMMIT_SHA" && "$SHORT_SHA" ]]; then
+    target="<!--GIT_COMMIT_LINK-->"
+    replace=" [<a href='https://github.com/GoogleCloudPlatform/microservices-demo/commit/${COMMIT_SHA}'\
+             style='font-size:80%'>${SHORT_SHA}</a>]#"
+
+    sed -i'' -e "s^$target^$replace^g" src/frontend/templates/footer.html
+
+    cat src/frontend/templates/footer.html
+fi
