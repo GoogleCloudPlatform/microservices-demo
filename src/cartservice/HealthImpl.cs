07c669a047b579694d882b0b3ff129f862a63610
askmeegs <megan037@gmail.com>
2019-08-18 15:33:58 -0400

    Increases cartservice health check rpc timeout

--- src/cartservice/HealthImpl.cs
+++ src/cartservice/HealthImpl.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using static System.Diagnostics.Stopwatch;
 using cartservice.interfaces;
 using Grpc.Core;
 using Grpc.Health.V1;
@@ -14,11 +15,14 @@ namespace cartservice {
         }
 
         public override Task<HealthCheckResponse> Check(HealthCheckRequest request, ServerCallContext context){
-            Console.WriteLine ("Checking CartService Health");
-
-            return Task.FromResult(new HealthCheckResponse {
+            var watch = StartNew();
+            var result = Task.FromResult(new HealthCheckResponse {
                 Status = dependency.Ping() ? HealthCheckResponse.Types.ServingStatus.Serving : HealthCheckResponse.Types.ServingStatus.NotServing
             });
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine ("✅ Health Check took " + elapsedMs + "ms");
+            return result;
         }
     }
 }
