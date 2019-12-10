450b1393b255c3eed95f1713afce9e59a8aeeefe
Steinwagner <matthias.steinwagner@sap.com>
2019-12-10 16:16:03 +0100

    Update DeviceController.java

--- src/tamagotchi-service/src/main/java/com/sap/tamagotchi/controller/DeviceController.java
+++ src/tamagotchi-service/src/main/java/com/sap/tamagotchi/controller/DeviceController.java
@@ -3,17 +3,17 @@ package com.sap.tamagotchi.controller;
 import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.sap.tamagotchi.model.Device;
-import com.sap.tamagotchi.publisher.PublisherService;
-import com.sap.tamagotchi.service.TamagotchiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sap.tamagotchi.model.Device;
+import com.sap.tamagotchi.service.TamagotchiService;
+
 @RestController
 public class DeviceController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello !!!!!, %s!";
     private final AtomicLong counter = new AtomicLong();
     private final TamagotchiService tamagotchiService;
 
@@ -31,8 +31,8 @@ public class DeviceController {
     public Collection<Device> getDevices() {
         return tamagotchiService.getDevices();
     }
-    //TODO postmapping create
-    //request payload
+    // TODO postmapping create
+    // request payload
     // owner String
     // color
 }
