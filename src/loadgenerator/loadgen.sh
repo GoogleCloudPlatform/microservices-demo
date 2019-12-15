1b57e37ee1c2a3322b80145226b5bbd1ed5d9e0a
Megan O'Keefe <megan037@gmail.com>
2019-12-15 16:08:23 -0500

    Adds liveness probe annotation, moves loadgen initcontainer into main

--- src/loadgenerator/loadgen.sh
+++ src/loadgenerator/loadgen.sh
@@ -18,10 +18,19 @@
 set -e
 trap "exit" TERM
 
-if [[ -z "${FRONTEND_ADDR}" ]]; then
+if [ -z "${FRONTEND_ADDR}" ]; then
     echo >&2 "FRONTEND_ADDR not specified"
     exit 1
 fi
 
 set -x
+
+# if one request to the frontend fails, then exit
+STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" http://${FRONTEND_ADDR})
+if test $STATUSCODE -ne 200; then
+    echo "Error: cannot reach frontend, exiting"
+    exit 1
+fi
+
+# else, run loadgen
 locust --host="http://${FRONTEND_ADDR}" --no-web -c "${USERS:-10}" 2>&1
