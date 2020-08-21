202f98701441e63aa088eee79fa3515aa64a4c68
Rahul Jagad <rahul.jagad@relyance.ai>
2020-08-20 17:51:39 -0700

    Integrate datadog APM integration for the online boutique app for relyance demo.

--- src/shippingservice/main.go
+++ src/shippingservice/main.go
@@ -32,7 +32,8 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
-
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	gt "gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/grpc"
 	pb "github.com/GoogleCloudPlatform/microservices-demo/src/shippingservice/genproto"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
@@ -58,6 +59,13 @@ func init() {
 }
 
 func main() {
+	
+	tracer.Start(
+		tracer.WithEnv("prod"),
+		tracer.WithService("shippingservice"),
+	)
+	defer tracer.Stop()
+
 	if os.Getenv("DISABLE_TRACING") == "" {
 		log.Info("Tracing enabled.")
 		go initTracing()
@@ -84,13 +92,24 @@ func main() {
 	}
 
 	var srv *grpc.Server
+	// Default stats/traces enabled
 	if os.Getenv("DISABLE_STATS") == "" {
 		log.Info("Stats enabled.")
 		srv = grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
+	} else if os.Getenv("DATADOG_APM_ENABLED") == "true" { //datadog traces enabled
+			// Create the server interceptor using the grpc trace package.
+			log.Info("Datadog tracing enabled.")	
+			grpc_option := gt.WithServiceName(os.Getenv("DD_SERVICE"))
+			unary_server_trace := gt.UnaryServerInterceptor(grpc_option)
+			stream_server_trace := gt.StreamServerInterceptor(grpc_option)
+			unary_trace := grpc.UnaryInterceptor(unary_server_trace)
+			stream_trace := grpc.StreamInterceptor(stream_server_trace)
+			srv = grpc.NewServer(unary_trace, stream_trace)	
 	} else {
-		log.Info("Stats disabled.")
-		srv = grpc.NewServer()
+			log.Info("Stats disabled.")
+			srv = grpc.NewServer()	
 	}
+
 	svc := &server{}
 	pb.RegisterShippingServiceServer(srv, svc)
 	healthpb.RegisterHealthServer(srv, svc)
