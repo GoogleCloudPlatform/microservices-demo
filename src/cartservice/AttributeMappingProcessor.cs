bd5e0927f7c9135f22952d414f014ea9001a4606
Justin Smith <157923+justindsmith@users.noreply.github.com>
2020-09-21 16:21:44 -0700

    Introduce errors to redis and external database cart access (#35)

--- src/cartservice/AttributeMappingProcessor.cs
+++ src/cartservice/AttributeMappingProcessor.cs
@@ -13,21 +13,79 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using OpenTelemetry;
 using OpenTelemetry.Trace;
 
-internal class AttrMappingProcessor: ActivityProcessor
+internal class AttrMappingProcessor : ActivityProcessor
 {
-    public AttrMappingProcessor() {}
 
-    public override void OnEnd(Activity activity)
+  private static double REDIS_SPAN_ERROR_RATE = Convert.ToDouble(Environment.GetEnvironmentVariable("REDIS_SPAN_ERROR_RATE"));
+  private static string EXTERNAL_DB_NAME = Environment.GetEnvironmentVariable("EXTERNAL_DB_NAME") ?? "global.datastore";
+  private static double EXTERNAL_DB_CHANGE_RATE = Convert.ToDouble(Environment.GetEnvironmentVariable("EXTERNAL_DB_CHANGE_RATE"));
+
+
+  public Random _random;
+
+
+  public AttrMappingProcessor()
+  {
+    _random = new Random();
+  }
+
+  public override void OnEnd(Activity activity)
+  {
+    foreach (var tag in activity.Tags)
     {
-        foreach (var tag in activity.Tags) {
-          if (tag.Key == "db.system") {
-            activity.AddTag("db.type", tag.Value);
-            break;
-          }
+      if (tag.Key == "db.system")
+      {
+        // Duplicate tag for reporting
+        activity.AddTag("db.type", tag.Value);
+
+        // Associate a random error some some redis calls
+        if (_random.NextDouble() < REDIS_SPAN_ERROR_RATE)
+        {
+          activity.AddTag("error", "true");
         }
+
+        break;
+      }
     }
+
+    // Change some percentage of the "redis HMSET" spans to represent an external call to another
+    // postgres database that does NOT have errors
+    Boolean changeToExternalDbSpan = _random.NextDouble() < EXTERNAL_DB_CHANGE_RATE;
+    if (
+      changeToExternalDbSpan &&
+      HasTagValue(activity.Tags, new KeyValuePair<string, string>("db.system", "redis")) && // redis
+      activity.DisplayName == "HMSET"                                                       // HMSET
+    )
+    {
+      // Change tags
+      activity.SetTag("db.system", "postgres");
+      activity.SetTag("db.type", "postgres");
+      activity.SetTag("peer.service", EXTERNAL_DB_NAME + ":98321");
+      activity.SetTag("error", "false");
+
+      // Change Operation
+      activity.DisplayName = "Database.Query";
+
+      // NOTE: Apparently changing the duration is not possible once an Activity has alrady been ended.
+      // Calling activity.SetEndTime() from here has no effect on the emitted span duration.
+    }
+  }
+
+  private static bool HasTagValue(IEnumerable<KeyValuePair<string, string>> tags, KeyValuePair<string, string> tag)
+  {
+    foreach (KeyValuePair<string, string> possibleTag in tags)
+    {
+      if (possibleTag.Key == tag.Key && possibleTag.Value == tag.Value)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
