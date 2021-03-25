1167a58fb79953142a973958e37975105747c2e5
deesejohn <deese.john@gmail.com>
2021-03-25 14:04:45 +0000

    Fix dotnet CLI tests for Github actions (#516)
    * Fix dotnet CLI tests for Github actions, Fixes #428
    
    * Updated Github workflows
    
    * Added setup-dotnet to workflows
    
    * Removing Urls from appsettings.json

--- src/cartservice/tests/CartServiceTests.cs
+++ src/cartservice/tests/CartServiceTests.cs
@@ -14,37 +14,53 @@
 
 using System;
 using System.Threading.Tasks;
-using Grpc.Core;
+using Grpc.Net.Client;
 using Hipstershop;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 using Xunit;
 using static Hipstershop.CartService;
 
-namespace cartservice
+namespace cartservice.tests
 {
-    public class E2ETests
+    public class CartServiceTests
     {
-        private static string serverHostName = "localhost";
-        private static int port = 7070;
+        private readonly IHostBuilder _host;
+
+        public CartServiceTests()
+        {
+            _host = new HostBuilder().ConfigureWebHost(webBuilder =>
+            {
+                webBuilder
+                    .UseStartup<Startup>()
+                    .UseTestServer();
+            });
+        }
 
         [Fact]
         public async Task GetItem_NoAddItemBefore_EmptyCartReturned()
         {
+            // Setup test server and client
+            using var server = await _host.StartAsync();
+            var httpClient = server.GetTestClient();
+
             string userId = Guid.NewGuid().ToString();
 
-            // Construct server's Uri
-            string targetUri = $"{serverHostName}:{port}";
-
             // Create a GRPC communication channel between the client and the server
-            var channel = new Channel(targetUri, ChannelCredentials.Insecure);
+            var channel = GrpcChannel.ForAddress(httpClient.BaseAddress, new GrpcChannelOptions
+            {
+                HttpClient = httpClient
+            });
 
-            var client = new CartServiceClient(channel);
+            var cartClient = new CartServiceClient(channel);
 
             var request = new GetCartRequest
             {
                 UserId = userId,
             };
 
-            var cart = await client.GetCartAsync(request);
+            var cart = await cartClient.GetCartAsync(request);
             Assert.NotNull(cart);
 
             // All grpc objects implement IEquitable, so we can compare equality with by-value semantics
@@ -54,14 +70,18 @@ namespace cartservice
         [Fact]
         public async Task AddItem_ItemExists_Updated()
         {
+            // Setup test server and client
+            using var server = await _host.StartAsync();
+            var httpClient = server.GetTestClient();
+
             string userId = Guid.NewGuid().ToString();
 
-            // Construct server's Uri
-            string targetUri = $"{serverHostName}:{port}";
-
             // Create a GRPC communication channel between the client and the server
-            var channel = new Channel(targetUri, ChannelCredentials.Insecure);
-            
+            var channel = GrpcChannel.ForAddress(httpClient.BaseAddress, new GrpcChannelOptions
+            {
+                HttpClient = httpClient
+            });
+
             var client = new CartServiceClient(channel);
             var request = new AddItemRequest
             {
@@ -78,7 +98,7 @@ namespace cartservice
 
             // Second add of existing product - quantity should be updated
             await client.AddItemAsync(request);
-            
+
             var getCartRequest = new GetCartRequest
             {
                 UserId = userId
@@ -90,20 +110,24 @@ namespace cartservice
             Assert.Equal(2, cart.Items[0].Quantity);
 
             // Cleanup
-            await client.EmptyCartAsync(new EmptyCartRequest{ UserId = userId });
+            await client.EmptyCartAsync(new EmptyCartRequest { UserId = userId });
         }
 
         [Fact]
         public async Task AddItem_New_Inserted()
         {
+            // Setup test server and client
+            using var server = await _host.StartAsync();
+            var httpClient = server.GetTestClient();
+
             string userId = Guid.NewGuid().ToString();
 
-            // Construct server's Uri
-            string targetUri = $"{serverHostName}:{port}";
-
             // Create a GRPC communication channel between the client and the server
-            var channel = new Channel(targetUri, ChannelCredentials.Insecure);
-            
+            var channel = GrpcChannel.ForAddress(httpClient.BaseAddress, new GrpcChannelOptions
+            {
+                HttpClient = httpClient
+            });
+
             // Create a proxy object to work with the server
             var client = new CartServiceClient(channel);
 
@@ -128,7 +152,7 @@ namespace cartservice
             Assert.Equal(userId, cart.UserId);
             Assert.Single(cart.Items);
 
-            await client.EmptyCartAsync(new EmptyCartRequest{ UserId = userId });
+            await client.EmptyCartAsync(new EmptyCartRequest { UserId = userId });
             cart = await client.GetCartAsync(getCartRequest);
             Assert.Empty(cart.Items);
         }
