15e6aa6dbd2d2d023bd985b58b3e7ef4e33cd4df
eastack <admin@eastack.me>
2021-05-27 18:51:14 +0800

    Delete the redundant shebang in genproto.sh

--- src/recommendationservice/genproto.sh
+++ src/recommendationservice/genproto.sh
@@ -14,9 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash
-set -e
-
 # script to compile python protos
 #
 # requires gRPC tools:
