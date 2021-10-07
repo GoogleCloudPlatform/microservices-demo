6fa6d44ee4fab4c5eabbac2ffaaecb457394437f
Dudi <dudicon@gmail.com>
2021-10-07 22:32:06 +0300

    set shippingservice label

--- src/shippingservice/main.go
+++ src/shippingservice/main.go
@@ -59,7 +59,7 @@ func init() {
 }
 
 func main() {
-	err := rook.Start(map[string]string{})
+	err := rook.Start(map[string]string{"app": "shippingservice"})
 	if err != nil {
 		log.Error(fmt.Sprintf("Got error while setting Rookout: %w", err))
 	}
