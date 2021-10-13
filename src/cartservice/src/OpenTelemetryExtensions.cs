bc23cdf681060fa1bf5f290166f097cfcbc2812b
Alan West <3676547+alanwest@users.noreply.github.com>
2021-10-13 13:45:07 -0700

    Use OpenTelemetry.Exporter.OpenTelemetryProtocol.Logs

--- src/cartservice/src/OpenTelemetryExtensions.cs
+++ src/cartservice/src/OpenTelemetryExtensions.cs
@@ -2,8 +2,8 @@ using System;
 using cartservice.cartstore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using OpenTelemetry;
 using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -17,8 +17,6 @@ namespace cartservice.OpenTelemetry
                 .CreateDefault()
                 .AddService("CartService")
                 .AddTelemetrySdk();
-
-        private static MeterProvider MeterProvider;
 
         public static void AddOpenTelemetry(this IServiceCollection services, ICartStore cartStore)
         {
@@ -36,11 +34,12 @@ namespace cartservice.OpenTelemetry
                     .AddOtlpExporter(ConfigureOtlpExporter);
             });
 
-            MeterProvider = Sdk.CreateMeterProviderBuilder()
-                .SetResourceBuilder(ResourceBuilder)
-                .AddAspNetCoreInstrumentation()
-                .AddOtlpExporter(ConfigureOtlpExporter)
-                .Build();
+            services.AddOpenTelemetryMetrics(builder => {
+                builder
+                    .SetResourceBuilder(ResourceBuilder)
+                    .AddAspNetCoreInstrumentation()
+                    .AddOtlpExporter(ConfigureOtlpExporter);
+            });
         }
 
         public static void ConfigureOpenTelemetry(this ILoggingBuilder builder)
@@ -48,15 +47,12 @@ namespace cartservice.OpenTelemetry
             builder
                 .AddOpenTelemetry(options =>
                 {
-                    var otlpExporterOptions = new OtlpExporterOptions
-                    {
-                        Endpoint = new Uri(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT"))
-                    };
+                    options.IncludeFormattedMessage = true;
 
                     options
                         .SetResourceBuilder(ResourceBuilder)
                         .AddProcessor(new SpanEventLogProcessor())
-                        .AddProcessor(new BatchLogRecordExportProcessor(new OtlpLogExporter(otlpExporterOptions)));
+                        .AddOtlpExporter(ConfigureOtlpExporter);
                 });
         }
 
