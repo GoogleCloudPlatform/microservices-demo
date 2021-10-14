dd15559c01283ef0120a334d132e592f3367e0f0
jxlwqq <jxlwqq@gmail.com>
2021-10-14 15:40:07 +0800

    Use context.WithTimeout instead of grpc.WithTimeout
    WithTimeout returns a DialOption that configures a timeout for dialing a ClientConn initially. This is valid if and only if WithBlock() is present.
    Deprecated: use DialContext and context.WithTimeout instead.

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -263,9 +263,10 @@ func mustMapEnv(target *string, envKey string) {
 
 func mustConnGRPC(ctx context.Context, conn **grpc.ClientConn, addr string) {
 	var err error
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
 	*conn, err = grpc.DialContext(ctx, addr,
 		grpc.WithInsecure(),
-		grpc.WithTimeout(time.Second*3),
 		grpc.WithStatsHandler(&ocgrpc.ClientHandler{}))
 	if err != nil {
 		panic(errors.Wrapf(err, "grpc: failed to connect %s", addr))
