73db41e7b1efc086f1b4b59964c74d0d706e4621
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2021-11-19 23:31:52 -0500

    Fix unit tests

--- src/cartservice/src/Startup.cs
+++ src/cartservice/src/Startup.cs
@@ -10,38 +10,63 @@ using Microsoft.Extensions.Hosting;
 using cartservice.cartstore;
 using cartservice.services;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace cartservice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-string redisAddress = builder.Configuration["REDIS_ADDR"];
-ICartStore cartStore = null;
-if (!string.IsNullOrEmpty(redisAddress))
-{
-    cartStore = new RedisCartStore(redisAddress);
-}
-else
-{
-    Console.WriteLine("Redis cache host(hostname+port) was not specified. Starting a cart service using local store");
-    Console.WriteLine("If you wanted to use Redis Cache as a backup store, you should provide its address via command line or REDIS_ADDR environment variable.");
-    cartStore = new LocalCartStore();
-}
-cartStore.InitializeAsync().GetAwaiter().GetResult();
-Console.WriteLine("Initialization completed");
-builder.Services.AddSingleton<ICartStore>(cartStore);
-builder.Services.AddGrpc();
+        public IConfiguration Configuration { get; }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string redisAddress = Configuration["REDIS_ADDR"];
+            ICartStore cartStore = null;
+            if (!string.IsNullOrEmpty(redisAddress))
+            {
+                cartStore = new RedisCartStore(redisAddress);
+            }
+            else
+            {
+                Console.WriteLine("Redis cache host(hostname+port) was not specified. Starting a cart service using local store");
+                Console.WriteLine("If you wanted to use Redis Cache as a backup store, you should provide its address via command line or REDIS_ADDR environment variable.");
+                cartStore = new LocalCartStore();
+            }
 
-var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
+            // Initialize the redis store
+            cartStore.InitializeAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Initialization completed");
+
+            services.AddSingleton<ICartStore>(cartStore);
+
+            services.AddGrpc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<CartService>();
+                endpoints.MapGrpcService<cartservice.services.HealthCheckService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
 }
-app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGrpcService<CartService>();
-    endpoints.MapGrpcService<cartservice.services.HealthCheckService>();
-});
-app.MapGet("/", async context =>
-{
-    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-});
-app.Run();
