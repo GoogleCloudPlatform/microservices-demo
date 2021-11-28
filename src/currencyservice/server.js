b939f02b6dafc92c4210f709d92e17693a3ed903
shabirmean <shabirmean@google.com>
2021-11-28 18:45:41 -0500

    test: add version to profiler

--- src/currencyservice/server.js
+++ src/currencyservice/server.js
@@ -22,7 +22,7 @@ else {
   require('@google-cloud/profiler').start({
     serviceContext: {
       service: 'currencyservice',
-      version: '1.0.0'
+      version: 'add-max-retry-pr642'
     },
   });
 }
