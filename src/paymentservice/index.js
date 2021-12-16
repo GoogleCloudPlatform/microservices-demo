67ef5d96abf19166a91b0eaeb041d38cd6ef3c3f
askmeegs <megan037@gmail.com>
2021-12-16 13:56:13 -0500

    Turn off GCP integrations by default

--- src/paymentservice/index.js
+++ src/paymentservice/index.js
@@ -16,19 +16,43 @@
 
 'use strict';
 
-require('@google-cloud/profiler').start({
-  serviceContext: {
-    service: 'paymentservice',
-    version: '1.0.0'
-  }
-});
-require('@google-cloud/trace-agent').start();
-require('@google-cloud/debug-agent').start({
-  serviceContext: {
-    service: 'paymentservice',
-    version: 'VERSION'
-  }
-});
+
+if(process.env.DISABLE_PROFILER) {
+  console.log("Profiler disabled.")
+}
+else {
+  console.log("Profiler enabled.")
+  require('@google-cloud/profiler').start({
+    serviceContext: {
+      service: 'paymentservice',
+      version: '1.0.0'
+    }
+  });
+}
+
+
+if(process.env.DISABLE_TRACING) {
+  console.log("Tracing disabled.")
+}
+else {
+  console.log("Tracing enabled.")
+  require('@google-cloud/trace-agent').start();
+
+}
+
+if(process.env.DISABLE_DEBUGGER) {
+  console.log("Debugger disabled.")
+}
+else {
+  console.log("Debugger enabled.")
+  require('@google-cloud/debug-agent').start({
+    serviceContext: {
+      service: 'paymentservice',
+      version: 'VERSION'
+    }
+  });
+}
+
 
 const path = require('path');
 const HipsterShopServer = require('./server');
