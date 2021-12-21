2dfd53de042813846309a9c03c82c19f6a6d3961
Nim Jayawardena <nimjay@google.com>
2021-12-21 10:42:43 -0500

    Update docs & resources for GCP org migration (#659)
    * Update release manifests for GCP org change
    
    * Update release steps for GCP org migration
    
    * Fix install-dependencies.sh
    
    Co-authored-by: askmeegs <megan037@gmail.com>

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -37,6 +37,21 @@ echo "✅ dotnet installed"
 sudo apt-get install -yqq kubectl git
 echo "✅ kubectl installed"
 
+# install go
+wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+echo 'export GOPATH=$HOME/go' >> ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> ~/.profile
+source ~/.profile
+echo "✅ golang installed"
+
+# install build-essential (gcc, used for go test)
+sudo apt install -y build-essential
+
+# install addlicense
+go install github.com/google/addlicense@latest
+sudo ln -s $HOME/go/bin/addlicense /bin
+
 # install build-essential (gcc, used for go test)
 sudo apt install -y build-essential
 
