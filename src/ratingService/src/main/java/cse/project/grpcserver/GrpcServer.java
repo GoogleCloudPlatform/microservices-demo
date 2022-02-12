60406258212d2b502bba7b071e2ab341b1c63520
blablablorges <mb@ise.tu-berlin>
2022-02-12 18:51:06 +0000

    fix failing liveness and readiness probes

--- src/ratingService/src/main/java/cse/project/grpcserver/GrpcServer.java
+++ src/ratingService/src/main/java/cse/project/grpcserver/GrpcServer.java
@@ -3,16 +3,21 @@ package cse.project.grpcserver;
 import java.io.IOException;
 
 import cse.project.ratingservice.RatingService;
+import io.grpc.health.v1.HealthCheckResponse.ServingStatus;
+import io.grpc.services.*;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class GrpcServer {
-
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		Server server = ServerBuilder.forPort(9090).addService(new RatingService()).build();
+
+		Server server;
+		HealthStatusManager healthMgr;
+		healthMgr = new HealthStatusManager();
+		server = ServerBuilder.forPort(9090).addService(new RatingService()).addService(healthMgr.getHealthService()).build();
 		server.start();
 		System.out.print("Service started at "+server.getPort());
+		healthMgr.setStatus("", ServingStatus.SERVING);
 		server.awaitTermination();
 	}
 
