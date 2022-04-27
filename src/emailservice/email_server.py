ef8ed8da49b3a09dd85e8c1089d6e30a9c014012
jack-berg <jberg@newrelic.com>
2022-04-26 19:13:50 -0500

    Email agent uses new relic python agent

--- src/emailservice/email_server.py
+++ src/emailservice/email_server.py
@@ -14,43 +14,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
-import os
-import sys
-import time
-from concurrent import futures
+# Install new relic agent
+import newrelic.agent
+newrelic.agent.initialize('/email_server/newrelic.ini')
 
+from concurrent import futures
+import os
+import time
 import grpc
-import traceback
 from jinja2 import Environment, FileSystemLoader, select_autoescape, TemplateError
 from google.api_core.exceptions import GoogleAPICallError
-from grpc_health.v1 import health_pb2, health_pb2_grpc
-from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.instrumentation.grpc import GrpcInstrumentorServer
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 import demo_pb2
 import demo_pb2_grpc
 from grpc_health.v1 import health_pb2
 from grpc_health.v1 import health_pb2_grpc
 
+
 from logger import getJSONLogger
+logger = getJSONLogger('emailservice-server')
 
-logger = getJSONLogger("emailservice-server")
-
-grpc_server_instrumentor = GrpcInstrumentorServer()
-grpc_server_instrumentor.instrument()
-otlp_exporter = OTLPSpanExporter()
+# try:
+#     googleclouddebugger.enable(
+#         module='emailserver',
+#         version='1.0.0'
+#     )
+# except:
+#     pass
 
 # Loads confirmation email template from file
 env = Environment(
-    loader=FileSystemLoader("templates"), autoescape=select_autoescape(["html", "xml"])
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape(['html', 'xml'])
 )
-template = env.get_template("confirmation.html")
-
+template = env.get_template('confirmation.html')
 
 class BaseEmailService(demo_pb2_grpc.EmailServiceServicer):
   def Check(self, request, context):
@@ -61,102 +58,86 @@ class BaseEmailService(demo_pb2_grpc.EmailServiceServicer):
     return health_pb2.HealthCheckResponse(
       status=health_pb2.HealthCheckResponse.UNIMPLEMENTED)
 
-    def Watch(self, request, context):
-        return health_pb2.HealthCheckResponse(
-            status=health_pb2.HealthCheckResponse.UNIMPLEMENTED
-        )
-
-
 class EmailService(BaseEmailService):
-    def __init__(self):
-        raise Exception("cloud mail client not implemented")
-        super().__init__()
+  def __init__(self):
+    raise Exception('cloud mail client not implemented')
+    super().__init__()
 
-    @staticmethod
-    def send_email(client, email_address, content):
-        response = client.send_message(
-            sender=client.sender_path(project_id, region, sender_id),
-            envelope_from_authority="",
-            header_from_authority="",
-            envelope_from_address=from_address,
-            simple_message={
-                "from": {
-                    "address_spec": from_address,
-                },
-                "to": [{"address_spec": email_address}],
-                "subject": "Your Confirmation Email",
-                "html_body": content,
-            },
-        )
-        logger.info("Message sent: {}".format(response.rfc822_message_id))
+  @staticmethod
+  def send_email(client, email_address, content):
+    response = client.send_message(
+      sender = client.sender_path(project_id, region, sender_id),
+      envelope_from_authority = '',
+      header_from_authority = '',
+      envelope_from_address = from_address,
+      simple_message = {
+        "from": {
+          "address_spec": from_address,
+        },
+        "to": [{
+          "address_spec": email_address
+        }],
+        "subject": "Your Confirmation Email",
+        "html_body": content
+      }
+    )
+    logger.info("Message sent: {}".format(response.rfc822_message_id))
 
-    def SendOrderConfirmation(self, request, context):
-        email = request.email
-        order = request.order
+  def SendOrderConfirmation(self, request, context):
+    email = request.email
+    order = request.order
 
-        try:
-            confirmation = template.render(order=order)
-        except TemplateError as err:
-            context.set_details(
-                "An error occurred when preparing the confirmation mail."
-            )
-            logger.error(err.message)
-            context.set_code(grpc.StatusCode.INTERNAL)
-            return demo_pb2.Empty()
+    try:
+      confirmation = template.render(order = order)
+    except TemplateError as err:
+      context.set_details("An error occurred when preparing the confirmation mail.")
+      logger.error(err.message)
+      context.set_code(grpc.StatusCode.INTERNAL)
+      return demo_pb2.Empty()
 
-        try:
-            EmailService.send_email(self.client, email, confirmation)
-        except GoogleAPICallError as err:
-            context.set_details("An error occurred when sending the email.")
-            print(err.message)
-            context.set_code(grpc.StatusCode.INTERNAL)
-            return demo_pb2.Empty()
+    try:
+      EmailService.send_email(self.client, email, confirmation)
+    except GoogleAPICallError as err:
+      context.set_details("An error occurred when sending the email.")
+      print(err.message)
+      context.set_code(grpc.StatusCode.INTERNAL)
+      return demo_pb2.Empty()
 
-        return demo_pb2.Empty()
-
+    return demo_pb2.Empty()
 
 class DummyEmailService(BaseEmailService):
-    def SendOrderConfirmation(self, request, context):
-        logger.info(
-            "A request to send order confirmation email to {} has been received.".format(
-                request.email
-            )
-        )
-        return demo_pb2.Empty()
+  def SendOrderConfirmation(self, request, context):
+    logger.info('A request to send order confirmation email to {} has been received.'.format(request.email))
+    return demo_pb2.Empty()
 
-
-class HealthCheck:
-    def Check(self, request, context):
-        return health_pb2.HealthCheckResponse(
-            status=health_pb2.HealthCheckResponse.SERVING
-        )
-
+class HealthCheck():
+  def Check(self, request, context):
+    return health_pb2.HealthCheckResponse(
+      status=health_pb2.HealthCheckResponse.SERVING)
 
 def start(dummy_mode):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    service = None
-    if dummy_mode:
-        service = DummyEmailService()
-    else:
-        raise Exception("non-dummy mode not implemented yet")
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),)
+  service = None
+  if dummy_mode:
+    service = DummyEmailService()
+  else:
+    raise Exception('non-dummy mode not implemented yet')
 
-    demo_pb2_grpc.add_EmailServiceServicer_to_server(service, server)
-    health_pb2_grpc.add_HealthServicer_to_server(service, server)
+  demo_pb2_grpc.add_EmailServiceServicer_to_server(service, server)
+  health_pb2_grpc.add_HealthServicer_to_server(service, server)
 
-    port = os.environ.get("PORT", "8080")
-    logger.info("listening on port: " + port)
-    server.add_insecure_port("[::]:" + port)
-    server.start()
-    try:
-        while True:
-            time.sleep(3600)
-    except KeyboardInterrupt:
-        server.stop(0)
+  port = os.environ.get('PORT', "8080")
+  logger.info("listening on port: "+port)
+  server.add_insecure_port('[::]:'+port)
+  server.start()
+  try:
+    while True:
+      time.sleep(3600)
+  except KeyboardInterrupt:
+    server.stop(0)
 
 
-if __name__ == "__main__":
-    logger.info("starting the email service in dummy mode.")
-    resource = Resource(attributes={"service.name": "emailservice"})
-    trace.set_tracer_provider(TracerProvider(resource=resource))
-    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
-    start(dummy_mode=True)
+if __name__ == '__main__':
+  logger.info('starting the email service in dummy mode.')
+  
+  start(dummy_mode = True)
