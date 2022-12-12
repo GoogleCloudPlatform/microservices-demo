993ccd62c6d9f95b4be6f683950d8d3a441d78b1
Mahima <89216134+mahi072@users.noreply.github.com>
2022-12-12 22:38:43 +0530

    issue #703: created a new/separate logger.go file

---
+++ src/frontend/logger.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func initializeLogger() {
+	log = logrus.New()
+	log.Level = logrus.DebugLevel
+	log.Formatter = &logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "timestamp",
+			logrus.FieldKeyLevel: "severity",
+			logrus.FieldKeyMsg:   "message",
+		},
+		TimestampFormat: time.RFC3339Nano,
+	}
+	log.Out = os.Stdout
+}
