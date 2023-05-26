5719519b964782044f09318dbd44fc979f920cd0
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-05-26 12:07:28 +0200

    [cartservice] Update dependencies (#123)

--- src/cartservice/genproto.bat
+++ src/cartservice/genproto.bat
@@ -20,8 +20,8 @@ setlocal
 cd /d %~dp0
 
 set NUGET_PATH=%UserProfile%\.nuget\packages
-set TOOLS_PATH=%NUGET_PATH%\Grpc.Tools\1.12.0\tools\windows_x64
+set TOOLS_PATH=%NUGET_PATH%\Grpc.Tools\2.54.0\tools\windows_x64
 
-%TOOLS_PATH%\protoc.exe -I%~dp0/../../pb;%NUGET_PATH%\google.protobuf.tools\3.5.1\tools\ --csharp_out %~dp0\grpc_generated %~dp0\..\..\pb\demo.proto --grpc_out %~dp0\grpc_generated --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
+%TOOLS_PATH%\protoc.exe -I%~dp0/../../pb;%NUGET_PATH%\google.protobuf.tools\3.23.1\tools\ --csharp_out %~dp0\grpc_generated %~dp0\..\..\pb\demo.proto --grpc_out %~dp0\grpc_generated --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
 
 endlocal
