55bf7b6c096d5c8a71cffc82b9de1d5dbb4b8d51
ahmadabed <ahmad.abed2000@gmail.com>
2023-08-20 15:18:09 +0300

    add integration tests

---
+++ integration-tests/go-tests/go.mod
@@ -0,0 +1,27 @@
+module main.go
+
+go 1.20
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/c2h5oh/datasize v0.0.0-20220606134207-859f65c6625b // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/influxdata/tdigest v0.0.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.1 // indirect
+	github.com/rs/dnscache v0.0.0-20230804202142-fc85eb664529 // indirect
+	github.com/tsenart/go-tsz v0.0.0-20180814235614-0bd30b3df1c3 // indirect
+	github.com/tsenart/vegeta v12.7.0+incompatible // indirect
+	github.com/tsenart/vegeta/v12 v12.11.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ integration-tests/new-java-tests/src/test/java/org/example/LoadTest.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoadTest {
+
+    private CloseableHttpClient httpClient;
+
+    @Before
+    public void setUp() {
+        httpClient = HttpClients.createDefault();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        httpClient.close();
+    }
+
+    @Test
+    public void testIndex() throws IOException {
+        HttpGet request = new HttpGet("http://10.2.10.105:8080/");
+        HttpResponse response = httpClient.execute(request);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void testSetCurrency() throws IOException {
+        String[] currencies = {"EUR", "USD", "JPY", "CAD"};
+        String currency = currencies[new Random().nextInt(currencies.length)];
+
+        HttpGet request = new HttpGet("http://10.2.10.105:8080/setCurrency?currency_code=" + currency);
+        HttpResponse response = httpClient.execute(request);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void testBrowseProduct() throws IOException {
+        String[] products = {
+                "0PUK6V6EV0",
+                "1YMWWN1N4O",
+                "2ZYFJ3GM2N",
+                "66VCHSJNUP",
+                "6E92ZMYYFZ",
+                "9SIQT8TOJO",
+                "L9ECAV7KIM",
+                "LS4PSXUNUM",
+                "OLJCESPC7Z"
+        };
+
+        String product = products[new Random().nextInt(products.length)];
+
+        HttpGet request = new HttpGet("http://10.2.10.105:8080/product/" + product);
+        HttpResponse response = httpClient.execute(request);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    // Add more test methods for other stages (addToCart, viewCart, checkout)
+}
