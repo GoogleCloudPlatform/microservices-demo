784b5b7b56784eb2b1c9ae65f01ae0970b6ed3dc
AhmadSealights <ahmad.wahbi@sealights.io>
2023-08-22 13:51:03 +0300

    did checkout service

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -2,8 +2,8 @@
 # Check if sealights folder exists
 if [ -d "/app/sealights/" ]; then
     /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName master --buildName cartservice1.1.3 \
-                  --binDir /app --includeNamespace *icartservice.services*  --target $1 --tokenFile sltoken.txt  --workingDir /app \
-                  --identifyMethodsByFqn --profilerLogLevel 7 --profilerLogDir /app/sealights/logs --labId integ_main_GoogleMicroserviceDemo
+                  --binDir /app --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
+                  --identifyMethodsByFqn --profilerLogLevel 7 --profilerLogDir /app/sealights/logs --labId ${SL_LAB_ID}
 else
     dotnet $1
 fi
