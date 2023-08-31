60532fa1955db4c8a40a03aad2ef7faaad49c2c5
ahmadabed <ahmad.abed2000@gmail.com>
2023-08-31 16:17:45 +0300

    update jenkinsfile btq-build and test files

--- integration-tests/nodejs-tests/mocha/node_modules/@ungap/promise-all-settled/min.js
+++
@@ -1,1 +0,0 @@
-var allSettled=Promise.allSettled||function(t){"use strict";var e=this;return e.all(t.map(function(t){return e.resolve(t).then(this.$,this._)},{$:function(t){return{status:"fulfilled",value:t}},_:function(t){return{status:"rejected",reason:t}}}))};
