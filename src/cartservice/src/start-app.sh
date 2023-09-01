64b19fec9eb3101d6069bf652401108a73576685
ahmadabed1 <73169815+ahmadabed1@users.noreply.github.com>
2023-09-01 11:12:25 +0300

    Update start-app.sh

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -3,7 +3,7 @@
 if [ -d "/app/sealights/" ]; then
     /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName 0.1 \
                   --binDir /app --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
-                  --identifyMethodsByFqn --profilerLogLevel 7 --profilerLogDir /app/sealights/logs --labId ${SL_LAB_ID}
+                  --identifyMethodsByFqn --profilerLogLevel 7 --profilerLogDir /app/sealights/logs --labId ${SL_LABID}
 else
     dotnet $1
 fi
