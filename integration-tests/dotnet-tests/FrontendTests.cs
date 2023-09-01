6d5ffe869ceb50a0565d11e4596ccea8d14e94a2
ahmadabed1 <73169815+ahmadabed1@users.noreply.github.com>
2023-09-01 10:40:21 +0300

    Update FrontendTests.cs

--- integration-tests/dotnet-tests/FrontendTests.cs
+++ integration-tests/dotnet-tests/FrontendTests.cs
@@ -27,7 +27,7 @@ namespace FrontendTests.Tests
         [TestInitialize]
         public void Setup()
         {
-            client = new HttpClient { BaseAddress = new Uri(Environment.GetEnvironmentVariable("machine_dns") ?? "http://10.2.10.163:8081") };
+            client = new HttpClient { BaseAddress = new Uri(Environment.GetEnvironmentVariable("machine_dns") ?? "machine_dns") };
         }
 
         [TestMethod]
