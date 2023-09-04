b8cfdf97c2009583791793dbc73eed45657a8e4c
ahmadabed <ahmad.abed2000@gmail.com>
2023-09-04 09:44:57 +0300

    update package.json and jenkinsfile

--- integration-tests/nodejs-tests/new-pro/cypress/cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-// cypress/plugins/index.js
-
-module.exports = (on, config) => {
-  // Set environment variables for your tests here
-  config.env.machine_dns = 'http://10.2.10.163:8081';
-
-  // Return the updated configuration
-  return config;
-};
--- integration-tests/nodejs-tests/new-pro/cypress/cypress/support/index.js
+++
@@ -1,4 +0,0 @@
-// cypress/support/index.js
-
-import './commands'; // Import and include your custom commands here
-import "SL.Cypress.Plugin/dist/code-coverage/support";
