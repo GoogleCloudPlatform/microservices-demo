112ac69024a62522186f6bc93a2be616559c26f1
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-05 09:02:22 +0300

    attempt 1

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -3,7 +3,7 @@
 if [ -d "/app/sealights/" ]; then
     /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName 0.4 \
                   --binDir /app --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
-                  --identifyMethodsByFqn --profilerLogLevel 7 --profilerLogDir /app/sealights/logs --labId ${SL_LABID}
+                  --identifyMethodsByFqn --labId ${SL_LAB_ID}
 else
     dotnet $1
 fi
