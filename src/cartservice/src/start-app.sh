680edac1fc0b8912481d428904d7295b4016c918
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-05 11:01:08 +0300

    .

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Check if sealights folder exists
 if [ -d "/app/sealights/" ]; then
-    /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName 0.4 \
+    /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName "0.4.1" --labId ${SL_LAB_ID} \
                   --binDir /app --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
-                  --identifyMethodsByFqn --labId ${SL_LAB_ID}
+                  --identifyMethodsByFqn --profilerLogLevel 7 --profilerLogDir /app/sealights/logs
 else
     dotnet $1
 fi
