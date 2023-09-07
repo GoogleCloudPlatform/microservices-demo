722a6f6297a841c57b779289004ef277b6fb2292
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-07 14:53:26 +0300

    fixed adservice

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -3,7 +3,7 @@
 if [ -d "/app/sealights/" ]; then
     /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName "0.4.1" --labId ${SL_LAB_ID} \
                   --binDir /app --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
-                  --identifyMethodsByFqn --profilerLogLevel 7 --profilerLogDir /app/sealights/logs
+                  --identifyMethodsByFqn
 else
     dotnet $1
 fi
