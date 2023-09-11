9dd2d360a78222de873e77520efd05dc4e08b84a
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-11 11:48:00 +0300

    fix adservice

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -2,8 +2,8 @@
 # Check if sealights folder exists
 if [ -d "/app/sealights/" ]; then
     /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName "0.4.1" --labId ${SL_LAB_ID} \
-                  --binDir /app/cartservice --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
+                  --binDir /app --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
                   --identifyMethodsByFqn
 else
-    dotnet $1
+    echo "there was an error"
 fi
