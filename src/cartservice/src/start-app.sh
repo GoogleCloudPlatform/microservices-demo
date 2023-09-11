d8295bb682f6ce863fd09ee83a1a748f3e40f347
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-11 13:54:53 +0300

    check --disable-on-init + fix cartservice

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Check if sealights folder exists
 if [ -d "/app/sealights/" ]; then
-    /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName "0.4.1" --labId ${SL_LAB_ID} \
-                  --binDir /app --includeNamespace *icartservice.services*  --target $1 --token ${SL_TOKEN}  --workingDir /app \
-                  --identifyMethodsByFqn
+  /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName medhat-ya-manyak \
+         --binDir /app  --includeNamespace *.services* --target $1 --token ${SL_TOKEN} --workingDir /app \
+         --identifyMethodsByFqn --labId ${SL_LAB_ID}
 else
-    echo "there was an error"
+  dotnet $1
 fi
