fa1012944b3e95f0f00aa74d6c461ee545b6e1ab
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-13 11:21:31 +0300

    try without cd buildnames

--- src/cartservice/src/start-app.sh
+++ src/cartservice/src/start-app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Check if sealights folder exists
 if [ -d "/app/sealights/" ]; then
-  /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName ${BUILD_NAME} \
+  /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName main --buildName test \
          --binDir /app  --includeNamespace *.services* --target $1 --token ${SL_TOKEN} --workingDir /app \
          --identifyMethodsByFqn --labId ${SL_LAB_ID}
 else
