11bf4fa66c0a1481d58fbe9dbb5d66ef940c4d40
ahmadabed <ahmad.abed2000@gmail.com>
2023-09-14 15:39:31 +0300

    add soapUI  framework

---
+++ integration-tests/soapUI/testrunner.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+### ====================================================================== ###
+##                                                                          ##
+##  SoapUI TestCaseRunner Bootstrap Script                                  ##
+##                                                                          ##
+### ====================================================================== ###
+
+### $Id$ ###
+
+DIRNAME=`dirname $0`
+
+# OS specific support (must be 'true' or 'false').
+cygwin=false;
+case "`uname`" in
+    CYGWIN*)
+        cygwin=true
+        ;;
+esac
+
+# Setup SOAPUI_HOME
+if [ "x$SOAPUI_HOME" = "x" ]
+then
+    # get the full path (without any relative bits)
+    SOAPUI_HOME=`cd $DIRNAME/..; pwd`
+fi
+export SOAPUI_HOME
+
+SOAPUI_CLASSPATH=$SOAPUI_HOME/bin/soapui-5.7.1.jar:$SOAPUI_HOME/lib/*
+JFXRTPATH=`java -cp $SOAPUI_CLASSPATH com.eviware.soapui.tools.JfxrtLocator`
+SOAPUI_CLASSPATH=$JFXRTPATH:$SOAPUI_CLASSPATH
+
+export SOAPUI_HOME
+
+JAVA_OPTS="-Xms128m -Xmx1024m -Dsoapui.properties=soapui.properties -Dsoapui.home=$SOAPUI_HOME/bin"
+
+#CVE-2021-44228
+JAVA_OPTS="$JAVA_OPTS -Dlog4j2.formatMsgNoLookups=true"
+
+#JAVA 16
+JAVA_OPTS="$JAVA_OPTS --illegal-access=permit"
+
+if [ $SOAPUI_HOME != "" ]
+then
+    JAVA_OPTS="$JAVA_OPTS -Dsoapui.ext.libraries=$SOAPUI_HOME/bin/ext"
+    JAVA_OPTS="$JAVA_OPTS -Dsoapui.ext.listeners=$SOAPUI_HOME/bin/listeners"
+    JAVA_OPTS="$JAVA_OPTS -Dsoapui.ext.actions=$SOAPUI_HOME/bin/actions"
+fi
+
+export JAVA_OPTS
+# For Cygwin, switch paths to Windows format before running java
+if [ $cygwin = "true" ]
+then
+    SOAPUI_HOME=`cygpath --path --dos "$SOAPUI_HOME"`
+    SOAPUI_CLASSPATH=`cygpath --path --dos "$SOAPUI_CLASSPATH"`
+fi
+
+echo ================================
+echo =
+echo = SOAPUI_HOME = $SOAPUI_HOME
+echo =
+echo ================================
+#export LAB_ID="${params.sl_labid}"
+#export TOKEN="${params.sl_token}"
+#sed -i 's/machine_dns/machine_dns1/' testrunner.sh
+
+java -javaagent:sl-test-listener.jar -Dsl.token="${TOKEN}" -Dsl.labId="${LAB_ID}" -Dsl.testStage="Soapui Tests" $JAVA_OPTS -cp $SOAPUI_CLASSPATH com.eviware.soapui.tools.SoapUITestCaseRunner "$@"
+
