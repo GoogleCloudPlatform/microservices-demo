508ba55e0635ff20ca84fe87a20c37349ae32118
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-18 11:43:55 +0300

    reverted initcontainer to it's original

--- initContainers/dotnetInitContainer/start-app.sh
+++ initContainers/dotnetInitContainer/start-app.sh
@@ -2,7 +2,7 @@
 # Check if sealights folder exists
 if [ -d "/sealights/" ]; then
   /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName ${BRANCH} --buildName ${BUILD_NAME} \
-         --binDir /app  --includeNamespace *.services* --target $1 --token ${SL_TOKEN} --workingDir /app \
+         --binDir /app  --includeNamespace *.services* --target /app/cartservice --token ${SL_TOKEN} --workingDir /app \
          --identifyMethodsByFqn --labId ${SL_LAB_ID}
 else
   dotnet $1
