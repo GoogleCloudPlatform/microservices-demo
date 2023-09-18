17642429603826e6e3bfafc945671aa7658214f4
AhmadSealights <ahmad.wahbi@sealights.io>
2023-09-18 12:32:49 +0300

    fix cartservice

--- initContainers/dotnetInitContainer/start-app.sh
+++ initContainers/dotnetInitContainer/start-app.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Check if sealights folder exists
 if [ -d "/sealights/" ]; then
-  /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName ${BRANCH} --buildName ${BUILD_NAME} \
-         --binDir /app  --includeNamespace *.services* --target /app/cartservice --token ${SL_TOKEN} --workingDir /app \
+  /app/sealights/agent/SL.DotNet cdAgent --appName cartservice --branchName init --buildName ${BUILD_NAME} \
+         --binDir /app  --includeNamespace *.services* --target $1 --token ${SL_TOKEN} --workingDir /app \
          --identifyMethodsByFqn --labId ${SL_LAB_ID}
 else
   dotnet $1
