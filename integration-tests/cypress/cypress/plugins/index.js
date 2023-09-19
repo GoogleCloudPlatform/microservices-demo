67937aac5d787a522a5aa8bbb8784cb09fe562fa
ahmadabed1 <Ahmad.aabed@sealights.io>
2023-09-19 14:43:19 +0300

    Update index.js

--- integration-tests/cypress/cypress/plugins/index.js
+++ integration-tests/cypress/cypress/plugins/index.js
@@ -1,12 +1,15 @@
 // cypress/plugins/index.js
 
-
 const { registerSealightsTasks } = require('SL.Cypress.Plugin/dist/code-coverage/config');
 
 module.exports = (on, config) => {
   // Set environment variables for your tests here
- config.env.machine_dns = 'http://10.2.10.50:8081';
- registerSealightsTasks(on, config);
+  config.env.machine_dns = process.env.MACHINE_DNS || 'blabla'; // Use the environment variable if it's set, otherwise use the default value
+  registerSealightsTasks(on, config);
+
+  // Log the value of machine_dns (optional)
+  console.log('machine_dns:', config.env.machine_dns);
+
   // Return the updated configuration
   return config;
 };
