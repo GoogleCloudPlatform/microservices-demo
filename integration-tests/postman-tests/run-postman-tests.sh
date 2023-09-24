499524ad3c6de062ef0d214a88182191d52bf16d
Ahmad <ahmad.wahbi@sealights.io>
2023-09-24 09:34:56 +0300

    run tests

---
+++ integration-tests/postman-tests/run-postman-tests.sh
@@ -0,0 +1,5 @@
+sudo apt install npm
+npm install -g newman
+npm install -g newman-reporter-xunit
+#newman run sealights\ excersise.postman_collection.json --env-var machine_dns=$machine_dns
+newman run sealights\ excersise.postman_collection.json --env-var machine_dns=$machine_dns -r xunit --reporter-xunit-export './result.xml'
