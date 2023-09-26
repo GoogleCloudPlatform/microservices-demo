4d846a8d1374bd510f2d47789465eee04a77b7e7
Ahmad <ahmad.wahbi@sealights.io>
2023-09-26 07:20:37 +0300

    fix postman tests

--- integration-tests/postman-tests/run-postman-tests.sh
+++ integration-tests/postman-tests/run-postman-tests.sh
@@ -1,5 +1,5 @@
-sudo apt install npm
-npm install -g newman
-npm install -g newman-reporter-xunit
+#sudo apt install npm
+#npm install -g newman
+#npm install -g newman-reporter-xunit
 #newman run sealights\ excersise.postman_collection.json --env-var machine_dns=$machine_dns
-newman run sealights\ excersise.postman_collection.json --env-var machine_dns=$machine_dns -r xunit --reporter-xunit-export './result.xml'
+newman run sealights-excersise.postman_collection.json --env-var machine_dns=$machine_dns -r xunit --reporter-xunit-export './result.xml'
