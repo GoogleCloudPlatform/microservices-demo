12c8866f6e683e2893543626b216b80ec177944c
Pablo Chacin <pablochacin@gmail.com>
2023-10-11 17:37:18 +0200

    Set OTEL service name in payment service
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- src/paymentservice/index.js
+++ src/paymentservice/index.js
@@ -38,9 +38,15 @@ if(process.env.ENABLE_TRACING == "1") {
   const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
   const { registerInstrumentations } = require('@opentelemetry/instrumentation');
   const { OTLPTraceExporter } = require("@opentelemetry/exporter-otlp-grpc");
+  const { Resource } = require('@opentelemetry/resources');
+  const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
-  const provider = new NodeTracerProvider();
-  
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: process.env.OTEL_SERVICE_NAME || 'paymentservice',
+    }),
+  });
+
   const collectorUrl = process.env.COLLECTOR_SERVICE_ADDR
 
   provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({url: collectorUrl})));
