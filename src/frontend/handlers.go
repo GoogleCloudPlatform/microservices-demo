5114d0f3526710f538136e50b46fd5fc4f3d4139
Nim Jayawardena <nimjay@google.com>
2023-11-28 16:44:54 -0500

    Remove isPackagingServiceConfigured from template

--- src/frontend/handlers.go
+++ src/frontend/handlers.go
@@ -191,7 +191,7 @@ func (fe *frontendServer) productHandler(w http.ResponseWriter, r *http.Request)
 
 	// Fetch packaging info (weight/dimensions) of the product
 	// The packaging service is an optional microservice you can run as part of a Google Cloud demo.
-	packagingInfo := PackagingInfo{}
+	var packagingInfo *PackagingInfo = nil
 	if isPackagingServiceConfigured() {
 		packagingInfo, err = httpGetPackagingInfo(id)
 		if err != nil {
@@ -200,22 +200,21 @@ func (fe *frontendServer) productHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	if err := templates.ExecuteTemplate(w, "product", map[string]interface{}{
-		"session_id":                   sessionID(r),
-		"request_id":                   r.Context().Value(ctxKeyRequestID{}),
-		"ad":                           fe.chooseAd(r.Context(), p.Categories, log),
-		"user_currency":                currentCurrency(r),
-		"show_currency":                true,
-		"currencies":                   currencies,
-		"product":                      product,
-		"recommendations":              recommendations,
-		"cart_size":                    cartSize(cart),
-		"platform_css":                 plat.css,
-		"platform_name":                plat.provider,
-		"is_cymbal_brand":              isCymbalBrand,
-		"deploymentDetails":            deploymentDetailsMap,
-		"frontendMessage":              frontendMessage,
-		"packagingInfo":                packagingInfo,
-		"isPackagingServiceConfigured": isPackagingServiceConfigured(),
+		"session_id":        sessionID(r),
+		"request_id":        r.Context().Value(ctxKeyRequestID{}),
+		"ad":                fe.chooseAd(r.Context(), p.Categories, log),
+		"user_currency":     currentCurrency(r),
+		"show_currency":     true,
+		"currencies":        currencies,
+		"product":           product,
+		"recommendations":   recommendations,
+		"cart_size":         cartSize(cart),
+		"platform_css":      plat.css,
+		"platform_name":     plat.provider,
+		"is_cymbal_brand":   isCymbalBrand,
+		"deploymentDetails": deploymentDetailsMap,
+		"frontendMessage":   frontendMessage,
+		"packagingInfo":     packagingInfo,
 	}); err != nil {
 		log.Println(err)
 	}
