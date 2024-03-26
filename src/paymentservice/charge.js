7ed885183661439cd4b66995ab43cddd0a96df82
Rahul Guin <rguin@google.com>
2024-03-26 20:21:04 +0000

    UI changes for demo

--- src/paymentservice/charge.js
+++ src/paymentservice/charge.js
@@ -52,6 +52,13 @@ class ExpiredCreditCard extends CreditCardError {
   }
 }
 
+async function exampleFetch() {
+    const response = await fetch('http://google.com');
+    const json = await response.json();
+    logger.info(json);
+}
+
+
 /**
  * Verifies the credit card number and (pretend) charges the card.
  *
@@ -67,6 +74,18 @@ module.exports = function charge (request) {
     valid
   } = cardInfo.getCardDetails();
 
+  /*
+  const requestf = async () => {
+    const response = await fetch('http://google.com/');
+    const json = await response.json();
+    console.log("testing only: " + json);
+}
+
+requestf();
+*/
+
+  //exampleFetch();
+
   if (!valid) { throw new InvalidCreditCard(); }
 
   // Only VISA and mastercard is accepted, other card types (AMEX, dinersclub) will
@@ -79,8 +98,57 @@ module.exports = function charge (request) {
   const { credit_card_expiration_year: year, credit_card_expiration_month: month } = creditCard;
   if ((currentYear * 12 + currentMonth) > (year * 12 + month)) { throw new ExpiredCreditCard(cardNumber.replace('-', ''), month, year); }
 
-  logger.info(`Transaction processed: ${cardType} ending ${cardNumber.substr(-4)} \
+  logger.info(`Transaction info received : ${cardType} ending ${cardNumber.substr(-4)} \
     Amount: ${amount.currency_code}${amount.units}.${amount.nanos}`);
 
+  var data = {};
+  data.amount_money = {};
+  data.amount_money.amount = Number(amount.units) ;
+
+
+  data.amount_money.currency = amount.currency_code;
+  var timeNow = Date.now();
+  data.idempotency_key = timeNow.toString() + amount.currency_code + amount.units + amount.nanos;
+  data.source_id = "cnon:card-nonce-ok";
+
+  logger.info(`Transaction to be processed by  a pos:  ${cardType} ending ${cardNumber.substr(-4)} \
+    data: ${JSON.stringify(data)}`);
+
+  var response = {};
+   // Send the POST request using fetch
+  const requestFunc = async () => {
+  const response3 = await fetch("https://connect.squareupsandbox.com/v2/payments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Square-Version": "2024-02-22",
+      "Authorization": "Bearer EAAAlz9wqEc5MUwJ4Yrok9tTOv6X4lHEuSD8DOxGClgoHns8YWrnhpui2ZRcW7C2"
+    },
+    body: JSON.stringify(data)
+  });
+    console.log("testing only for new fetch: iresponse3: " + JSON.stringify(response3));
+      const jsonResp = await response3.json();
+    response = jsonResp;
+    console.log("testing only for new fetch: " + JSON.stringify(jsonResp));
+    return jsonResp;
+    }
+
+const result1 =  requestFunc().then(val => {
+    // got value here
+    console.log("val here " + JSON.stringify(val));
+    console.log("val here " + val.payment.id);
+    const result = val;
+    return { transaction_id: val.payment.id };
+}).catch(e => {
+    // error
+    console.log(e);
+});
+
+    console.log("testing only for new fetch:  successfully completed ");
+    console.log("testing only for new fetch:  successfully completed  response " + JSON.stringify(response));
+    console.log("testing only for new fetch:  successfully completed  result1 " + JSON.stringify(result1));
+
+    console.log("testing only for new fetch:  successfully completed before returning : " + JSON.stringify(data));
   return { transaction_id: uuidv4() };
+  //return { err };
 };
