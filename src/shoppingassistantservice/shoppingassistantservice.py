16d78cbb192118242165f2b086ea22e16d843c2f
Olivier Bourgeois <bourgeoisor@gmail.com>
2024-04-03 13:59:16 -0400

    Add disable-shopping-assistant component

--- src/shoppingassistantservice/shoppingassistantservice.py
+++ src/shoppingassistantservice/shoppingassistantservice.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2018 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
