72370db9bf3f9cf7cae33d2fa7ffb9602d72f1d8
Serghei C. <81874080+ciudinsg@users.noreply.github.com>
2024-10-31 22:49:08 +0200

    Update main.go
    Instrumenting the Frontend Microservice
    
    https://deploy.equinix.com/blog/tutorial-observability-best-practices-for-devops-engineers/

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -182,7 +182,9 @@ func main() {
 	r.HandleFunc(baseUrl + "/_healthz", func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "ok") })
 	r.HandleFunc(baseUrl + "/product-meta/{ids}", svc.getProductByID).Methods(http.MethodGet)
 	r.HandleFunc(baseUrl + "/bot", svc.chatBotHandler).Methods(http.MethodPost)
-
+  // Instrumenting the Frontend Microservice
+	r.Handle("/metrics", promhttp.Handler()) // Exposes Prometheus metrics
+	
 	var handler http.Handler = r
 	handler = &logHandler{log: log, next: handler}     // add logging
 	handler = ensureSessionID(handler)                 // add session ID
