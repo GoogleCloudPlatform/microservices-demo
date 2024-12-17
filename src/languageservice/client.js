2bad782a517866f10abb1d48807985e4c32290e6
Philip <philip.morgner@gmx.de>
2024-12-17 17:43:11 +0100

    start over

---
+++ src/languageservice/client.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const grpc = require("grpc");
+
+const config = require("./config.json");
+
+const PROTO_PATH = path.join(__dirname, "./proto/demo.proto");
+const PORT = process.env.PORT || config.service.port;
+
+const shopProto = grpc.load(PROTO_PATH).hipstershop;
+const client = new shopProto.LanguageService(
+  `localhost:${PORT}`,
+  grpc.credentials.createInsecure()
+);
+
+const request = {
+  targetLanguageCode: "de",
+  translationKey: "test",
+};
+
+client.getSupportedLanguages({}, (err, response) => {
+  if (err) {
+    console.error(`Error in getSupportedLanguages: ${err}`);
+  } else {
+    console.log(`Language codes: ${response.language_codes}`);
+  }
+});
+
+client.translate(request, (err, response) => {
+  if (err) {
+    console.error(`Error in translate: ${err}`);
+  } else {
+    console.log(
+      `Translate: ${request.translationKey} to ${response.translationResult}`
+    );
+  }
+});
