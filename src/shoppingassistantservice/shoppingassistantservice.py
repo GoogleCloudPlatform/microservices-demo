e2b335e6d077c4ddc3856d685390f762bc18d7f5
Zsolt Rappi <rappizsolt@gmail.com>
2025-07-18 10:20:41 +0200

    feat: add shoppingassitant service (#4)
    * feat: add shoppingassitant service
    
    * chore: update alloy values

--- src/shoppingassistantservice/shoppingassistantservice.py
+++ src/shoppingassistantservice/shoppingassistantservice.py
@@ -30,13 +30,16 @@ ALLOYDB_DATABASE_NAME = os.environ["ALLOYDB_DATABASE_NAME"]
 ALLOYDB_TABLE_NAME = os.environ["ALLOYDB_TABLE_NAME"]
 ALLOYDB_CLUSTER_NAME = os.environ["ALLOYDB_CLUSTER_NAME"]
 ALLOYDB_INSTANCE_NAME = os.environ["ALLOYDB_INSTANCE_NAME"]
-ALLOYDB_SECRET_NAME = os.environ["ALLOYDB_SECRET_NAME"]
 
-secret_manager_client = secretmanager_v1.SecretManagerServiceClient()
-secret_name = secret_manager_client.secret_version_path(project=PROJECT_ID, secret=ALLOYDB_SECRET_NAME, secret_version="latest")
-secret_request = secretmanager_v1.AccessSecretVersionRequest(name=secret_name)
-secret_response = secret_manager_client.access_secret_version(request=secret_request)
-PGPASSWORD = secret_response.payload.data.decode("UTF-8").strip()
+# Commented these out to use AlloyDB IAM authentication instead of password
+
+# ALLOYDB_SECRET_NAME = os.environ["ALLOYDB_SECRET_NAME"]
+
+# secret_manager_client = secretmanager_v1.SecretManagerServiceClient()
+# secret_name = secret_manager_client.secret_version_path(project=PROJECT_ID, secret=ALLOYDB_SECRET_NAME, secret_version="latest")
+# secret_request = secretmanager_v1.AccessSecretVersionRequest(name=secret_name)
+# secret_response = secret_manager_client.access_secret_version(request=secret_request)
+# PGPASSWORD = secret_response.payload.data.decode("UTF-8").strip()
 
 engine = AlloyDBEngine.from_instance(
     project_id=PROJECT_ID,
@@ -44,8 +47,8 @@ engine = AlloyDBEngine.from_instance(
     cluster=ALLOYDB_CLUSTER_NAME,
     instance=ALLOYDB_INSTANCE_NAME,
     database=ALLOYDB_DATABASE_NAME,
-    user="postgres",
-    password=PGPASSWORD
+    # user="postgres",
+    # password=PGPASSWORD
 )
 
 # Create a synchronous connection to our vectorstore
@@ -68,28 +71,28 @@ def create_app():
         prompt = request.json['message']
         prompt = unquote(prompt)
 
-        # Step 1 – Get a room description from Gemini-vision-pro
-        llm_vision = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
-        message = HumanMessage(
-            content=[
-                {
-                    "type": "text",
-                    "text": "You are a professional interior designer, give me a detailed decsription of the style of the room in this image",
-                },
-                {"type": "image_url", "image_url": request.json['image']},
-            ]
-        )
-        response = llm_vision.invoke([message])
-        print("Description step:")
-        print(response)
-        description_response = response.content
+        # # Step 1 – Get a room description from Gemini-vision-pro
+        # llm_vision = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+        # message = HumanMessage(
+        #     content=[
+        #         {
+        #             "type": "text",
+        #             "text": "You are a professional interior designer, give me a detailed decsription of the style of the room in this image",
+        #         },
+        #         {"type": "image_url", "image_url": request.json['image']},
+        #     ]
+        # )
+        # response = llm_vision.invoke([message])
+        # print("Description step:")    
+        # print(response)
+        # description_response = response.content
 
         # Step 2 – Similarity search with the description & user prompt
-        vector_search_prompt = f""" This is the user's request: {prompt} Find the most relevant items for that prompt, while matching style of the room described here: {description_response} """
+        vector_search_prompt = f""" This is the user's request: {prompt} Find the most relevant items for that prompt."""
         print(vector_search_prompt)
 
         docs = vectorstore.similarity_search(vector_search_prompt)
-        print(f"Vector search: {description_response}")
+        # print(f"Vector search: {description_response}")
         print(f"Retrieved documents: {len(docs)}")
         #Prepare relevant documents for inclusion in final prompt
         relevant_docs = ""
@@ -101,8 +104,8 @@ def create_app():
         # Step 3 – Tie it all together by augmenting our call to Gemini-pro
         llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
         design_prompt = (
-            f" You are an interior designer that works for Online Boutique. You are tasked with providing recommendations to a customer on what they should add to a given room from our catalog. This is the description of the room: \n"
-            f"{description_response} Here are a list of products that are relevant to it: {relevant_docs} Specifically, this is what the customer has asked for, see if you can accommodate it: {prompt} Start by repeating a brief description of the room's design to the customer, then provide your recommendations. Do your best to pick the most relevant item out of the list of products provided, but if none of them seem relevant, then say that instead of inventing a new product. At the end of the response, add a list of the IDs of the relevant products in the following format for the top 3 results: [<first product ID>], [<second product ID>], [<third product ID>] ")
+            f" You are a shopping assistant for Online Boutique. You are tasked with providing recommendations to a customer on what they should buy based on their request. \n"
+            f"Here is a list of products that are relevant to it: {relevant_docs} Specifically, this is what the customer has asked for, see if you can accommodate it: {prompt} Do your best to pick the most relevant item out of the list of products provided, but if none of them seem relevant, then say that instead of inventing a new product. At the end of the response, add a list of the IDs of the relevant products in the following format for the top 3 results: [<first product ID>], [<second product ID>], [<third product ID>] ")
         print("Final design prompt: ")
         print(design_prompt)
         design_response = llm.invoke(
