5abc4dc8b9e804a3d458b7b031e1ec8b9331d4fd
Nailia Shakirova <nailia@google.com>
2025-08-14 12:14:37 +0200

    add a new network creation and setting it as a cluster network

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,6 +36,17 @@ module "enable_google_apis" {
   activate_apis = concat(local.base_apis, var.memorystore ? local.memorystore_apis : [])
 }
 
+resource "google_compute_network" "online_boutique_vpc" {
+  name = "online_boutique_vpc"
+}
+
+resource "google_compute_subnetwork" "subnet1" {
+  name          = "subnet1-us-central1"
+  ip_cidr_range = "10.10.0.0/20"
+  region        = "us-central1"
+  network       = google_compute_network.online_boutique_vpc.id
+}
+
 # Create GKE cluster
 resource "google_container_cluster" "my_cluster" {
 
