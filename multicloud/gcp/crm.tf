81d985a432c6e7921c1b90743f0bf27ccaf88d64
Nailia Shakirova <nailia@google.com>
2025-08-18 09:43:27 +0200

    Fix yet another typo

--- multicloud/gcp/crm.tf
+++ multicloud/gcp/crm.tf
@@ -187,10 +187,10 @@ resource "google_compute_network_peering" "remote_to_ob" {
   export_custom_routes = false
 }
 
-# 7. Create VPC peering from online-boutique-vpc VPC to crm-vpc  
+# 7. Create VPC peering from online-boutique-vpc VPC to remote  
 resource "google_compute_network_peering" "ob_to_remote" {
-  name         = "ob-to-crm-peering"
-  network      = "projects/${var.peering_project_id}/global/networks/online-boutique-vpc"
+  name         = "ob-to-remote-peering"
+  network      = "projects/${var.project_id}/global/networks/online-boutique-vpc"
   peer_network = "projects/${var.peering_project_id}/global/networks/${var.peering_vpc_network}"
 
   import_custom_routes = false
