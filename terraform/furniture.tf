57b959df26a0f313ae7f0d49d3cbbd17f761ae92
Nailia Shakirova <nailia@google.com>
2025-09-12 15:34:05 +0200

    vpn fixing attempt 1

--- terraform/furniture.tf
+++ terraform/furniture.tf
@@ -218,114 +218,144 @@ resource "google_compute_router" "boutique_router" {
 }
 
 # --- VPN Tunnels ---
-# Tunnel 1: furniture-gateway Interface 0 to boutique-gateway Interface 0
-resource "google_compute_vpn_tunnel" "tunnel_a_to_b_if0" {
-  name                          = "tunnel-furniture-to-boutique-if0"
-  region                        = var.region
-  project                       = var.gcp_project_id
-  peer_gcp_gateway              = google_compute_ha_vpn_gateway.boutique_ha_gateway.id
-  shared_secret                 = random_id.vpn_shared_secret.hex
-  router                        = google_compute_router.furniture_router.id
-  vpn_gateway                   = google_compute_ha_vpn_gateway.furniture_ha_gateway.id
-  vpn_gateway_interface         = 0
-  local_traffic_selector        = ["0.0.0.0/0"]
-  remote_traffic_selector       = ["0.0.0.0/0"]
+resource "google_compute_ha_vpn_gateway" "ha_gateway1" {
+  region  = "us-central1"
+  name    = "ha-vpn-1"
+  network = google_compute_network.ob_network.id
 }
 
-# Tunnel 2: furniture-gateway Interface 1 to boutique-gateway Interface 1
-resource "google_compute_vpn_tunnel" "tunnel_a_to_b_if1" {
-  name                          = "tunnel-furniture-to-boutique-if1"
-  region                        = var.region
-  project                       = var.gcp_project_id
-  peer_gcp_gateway              = google_compute_ha_vpn_gateway.boutique_ha_gateway.id
-  shared_secret                 = random_id.vpn_shared_secret.hex
-  router                        = google_compute_router.furniture_router.id
-  vpn_gateway                   = google_compute_ha_vpn_gateway.furniture_ha_gateway.id
-  vpn_gateway_interface         = 1
-  local_traffic_selector        = ["0.0.0.0/0"]
-  remote_traffic_selector       = ["0.0.0.0/0"]
+resource "google_compute_ha_vpn_gateway" "ha_gateway2" {
+  region  = "us-central1"
+  name    = "ha-vpn-2"
+  network = google_compute_network.furniture_vpc.id
 }
 
-# --- BGP Sessions on furniture-router ---
-resource "google_compute_router_interface" "furniture_if0" {
-  name       = "if0-tunnel-a-b-if0"
-  router     = google_compute_router.furniture_router.name
-  region     = var.region
-  project    = var.gcp_project_id
+resource "google_compute_router" "router1" {
+  name    = "ha-vpn-router1"
+  region  = "us-central1"
+  network = google_compute_network.ob_network.name
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router" "router2" {
+  name    = "ha-vpn-router2"
+  region  = "us-central1"
+  network = google_compute_network.furniture_vpc.name
+  bgp {
+    asn = 64515
+  }
+}
+
+resource "google_compute_vpn_tunnel" "tunnel1" {
+  name                  = "ha-vpn-tunnel1"
+  region                = "us-central1"
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway1.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway2.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router1.id
+  vpn_gateway_interface = 0
+}
+
+resource "google_compute_vpn_tunnel" "tunnel2" {
+  name                  = "ha-vpn-tunnel2"
+  region                = "us-central1"
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway1.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway2.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router1.id
+  vpn_gateway_interface = 1
+}
+
+resource "google_compute_vpn_tunnel" "tunnel3" {
+  name                  = "ha-vpn-tunnel3"
+  region                = "us-central1"
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway2.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway1.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router2.id
+  vpn_gateway_interface = 0
+}
+
+resource "google_compute_vpn_tunnel" "tunnel4" {
+  name                  = "ha-vpn-tunnel4"
+  region                = "us-central1"
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway2.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway1.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router2.id
+  vpn_gateway_interface = 1
+}
+
+resource "google_compute_router_interface" "router1_interface1" {
+  name       = "router1-interface1"
+  router     = google_compute_router.router1.name
+  region     = "us-central1"
   ip_range   = "169.254.0.1/30"
-  vpn_tunnel = google_compute_vpn_tunnel.tunnel_a_to_b_if0.name
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel1.name
 }
 
-resource "google_compute_router_peer" "furniture_peer0" {
-  name                      = "peer0-boutique"
-  router                    = google_compute_router.furniture_router.name
-  region                    = var.region
-  project                   = var.gcp_project_id
+resource "google_compute_router_peer" "router1_peer1" {
+  name                      = "router1-peer1"
+  router                    = google_compute_router.router1.name
+  region                    = "us-central1"
   peer_ip_address           = "169.254.0.2"
-  peer_asn                  = google_compute_router.boutique_router.bgp[0].asn
-  interface                 = google_compute_router_interface.furniture_if0.name
-  enable_ipv6               = false
+  peer_asn                  = 64515
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router1_interface1.name
 }
 
-resource "google_compute_router_interface" "furniture_if1" {
-  name       = "if1-tunnel-a-b-if1"
-  router     = google_compute_router.furniture_router.name
-  region     = var.region
-  project    = var.gcp_project_id
-  ip_range   = "169.254.1.1/30"
-  vpn_tunnel = google_compute_vpn_tunnel.tunnel_a_to_b_if1.name
-}
-
-resource "google_compute_router_peer" "furniture_peer1" {
-  name                      = "peer1-boutique"
-  router                    = google_compute_router.furniture_router.name
-  region                    = var.region
-  project                   = var.gcp_project_id
-  peer_ip_address           = "169.254.1.2"
-  peer_asn                  = google_compute_router.boutique_router.bgp[0].asn
-  interface                 = google_compute_router_interface.furniture_if1.name
-  enable_ipv6               = false
-}
-
-# --- BGP Sessions on boutique-router ---
-resource "google_compute_router_interface" "boutique_if0" {
-  name       = "if0-tunnel-a-b-if0"
-  router     = google_compute_router.boutique_router.name
-  region     = var.region
-  project    = var.gcp_project_id
-  ip_range   = "169.254.0.2/30"
-  vpn_tunnel = google_compute_vpn_tunnel.tunnel_a_to_b_if0.name
-}
-
-resource "google_compute_router_peer" "boutique_peer0" {
-  name                      = "peer0-furniture"
-  router                    = google_compute_router.boutique_router.name
-  region                    = var.region
-  project                   = var.gcp_project_id
-  peer_ip_address           = "169.254.0.1"
-  peer_asn                  = google_compute_router.furniture_router.bgp[0].asn
-  interface                 = google_compute_router_interface.boutique_if0.name
-  enable_ipv6               = false
-  # Advertisement handled at the router level for boutique-router
-}
-
-resource "google_compute_router_interface" "boutique_if1" {
-  name       = "if1-tunnel-a-b-if1"
-  router     = google_compute_router.boutique_router.name
-  region     = var.region
-  project    = var.gcp_project_id
+resource "google_compute_router_interface" "router1_interface2" {
+  name       = "router1-interface2"
+  router     = google_compute_router.router1.name
+  region     = "us-central1"
   ip_range   = "169.254.1.2/30"
-  vpn_tunnel = google_compute_vpn_tunnel.tunnel_a_to_b_if1.name
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel2.name
 }
 
-resource "google_compute_router_peer" "boutique_peer1" {
-  name                      = "peer1-furniture"
-  router                    = google_compute_router.boutique_router.name
-  region                    = var.region
-  project                   = var.gcp_project_id
+resource "google_compute_router_peer" "router1_peer2" {
+  name                      = "router1-peer2"
+  router                    = google_compute_router.router1.name
+  region                    = "us-central1"
   peer_ip_address           = "169.254.1.1"
-  peer_asn                  = google_compute_router.furniture_router.bgp[0].asn
-  interface                 = google_compute_router_interface.boutique_if1.name
-  enable_ipv6               = false
-  # Advertisement handled at the router level for boutique-router
+  peer_asn                  = 64515
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router1_interface2.name
+}
+
+resource "google_compute_router_interface" "router2_interface1" {
+  name       = "router2-interface1"
+  router     = google_compute_router.router2.name
+  region     = "us-central1"
+  ip_range   = "169.254.0.2/30"
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel3.name
+}
+
+resource "google_compute_router_peer" "router2_peer1" {
+  name                      = "router2-peer1"
+  router                    = google_compute_router.router2.name
+  region                    = "us-central1"
+  peer_ip_address           = "169.254.0.1"
+  peer_asn                  = 64514
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router2_interface1.name
+}
+
+resource "google_compute_router_interface" "router2_interface2" {
+  name       = "router2-interface2"
+  router     = google_compute_router.router2.name
+  region     = "us-central1"
+  ip_range   = "169.254.1.1/30"
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel4.name
+}
+
+resource "google_compute_router_peer" "router2_peer2" {
+  name                      = "router2-peer2"
+  router                    = google_compute_router.router2.name
+  region                    = "us-central1"
+  peer_ip_address           = "169.254.1.2"
+  peer_asn                  = 64514
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router2_interface2.name
 }
