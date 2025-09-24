20de7e68f5995c8fa8375016a77c0ae99cadb31d
Hector-Tecmilenio <navarroendy19@gmail.com>
2025-09-23 19:32:49 -0600

    Update versions.tf
    Actualiza configuración de Terraform para usar AWS en lugar de Google Cloud

--- .github/terraform/versions.tf
+++ .github/terraform/versions.tf
@@ -17,9 +17,14 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 6.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # ajusta según la versión que necesites
     }
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
