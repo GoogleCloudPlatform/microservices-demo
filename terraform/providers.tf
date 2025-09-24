8943e145cdcd6b76225e71b5cd227fc9d329c363
Hector-Tecmilenio <navarroendy19@gmail.com>
2025-09-23 18:54:36 -0600

    Update providers.tf
    Actualizaciones

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,27 +1,16 @@
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# Configuración principal de Terraform
 terraform {
+  required_version = ">= 1.6.0"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.48.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.12"
     }
   }
 }
 
-provider "google" {
-  project = var.gcp_project_id
-  region  = var.region
+# Configura el proveedor AWS
+provider "aws" {
+  region = var.aws_region  
 }
+
