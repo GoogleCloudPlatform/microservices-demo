1d573831d2861fc34931bc02871580d90fd41935
Lourenco Trevenzolli <lourenco.trevenzolli@liferay.com>
2025-10-07 09:02:13 +0200

    Adding some code

--- src/cartservice/src/services/HealthCheckService.cs
+++ src/cartservice/src/services/HealthCheckService.cs
@@ -25,7 +25,7 @@ namespace cartservice.services
     {
         private ICartStore _cartStore { get; }
 
-        public HealthCheckService (ICartStore cartStore) 
+        public HealthCheckService (ICartStore cartStore)
         {
             _cartStore = cartStore;
         }
@@ -33,9 +33,34 @@ namespace cartservice.services
         public override Task<HealthCheckResponse> Check(HealthCheckRequest request, ServerCallContext context)
         {
             Console.WriteLine ("Checking CartService Health");
+            Console.WriteLine("Result" + Sum(1, 2) + Average(1, 2, 3, 4, 5));
             return Task.FromResult(new HealthCheckResponse {
                 Status = _cartStore.Ping() ? HealthCheckResponse.Types.ServingStatus.Serving : HealthCheckResponse.Types.ServingStatus.NotServing
             });
+        }
+
+        public int Sum(int a, int b)
+        {
+            int c; // review me!
+            return a + b;
+        }
+
+        // Generated Code
+        public double Average(params int[] numbers)
+        {
+            int c; // let's see if Gemini will review this...
+            if (numbers == null || numbers.Length == 0)
+            {
+                // prints a funny joke on system out
+                Console.WriteLine("This method will make the whole code base durty")
+                return 0;
+            }
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return (double)sum / numbers.Length;
         }
     }
 }
