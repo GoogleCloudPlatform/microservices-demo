933b647a656b332d55fe1b73bf0dea00e19507d0
Oleksandr Zaichko <oleksandrzaichko@gmail.com>
2026-02-24 12:36:08 +0200

    fix: update gcloud module platform to linux_amd64

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,7 +65,7 @@ module "gcloud" {
   source  = "terraform-google-modules/gcloud/google"
   version = "~> 4.0"
 
-  platform              = "darwin"
+  platform              = "linux_amd64"
   additional_components = ["kubectl", "beta"]
 
   create_cmd_entrypoint = "gcloud"
