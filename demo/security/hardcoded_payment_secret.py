c43f8e76662ddc0b28c505744cf713dc9c4d73de
Vinicius Wiesehofer <37457596+vwiesehofer@users.noreply.github.com>
2026-06-20 18:56:19 -0300

    Create hardcoded_payment_secret.py

---
+++ demo/security/hardcoded_payment_secret.py
@@ -0,0 +1,5 @@
+PAYMENT_GATEWAY_KEY = "pk_live_demo_payment_key_123456"
+PAYMENT_GATEWAY_SECRET = "sk_live_demo_secret_987654"
+
+def process_payment(amount):
+    print(f"Processing payment of {amount}")
